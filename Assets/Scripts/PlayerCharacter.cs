51ca5cd0709b1ceae94eecb6be1de595db94c7d2
Kathryn Keller <Kathryn.Keller@iam.local>
2018-09-19 15:13:43 -0500

    Added player script

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("yo."); //how you print to the console in unity
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
