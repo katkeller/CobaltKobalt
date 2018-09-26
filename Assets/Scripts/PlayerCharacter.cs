825c3e9446ffa644150c93c0983084f243523dc0
Kathryn Keller <Kathryn.Keller@iam.local>
2018-09-26 14:59:16 -0500

    Added Player Movement to Left

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -15,20 +15,33 @@ public class PlayerCharacter : MonoBehaviour
 
     private bool hasKey; //bools must be named as yes/no questions, i.e. "hasKey" not "key."
     private bool isOnGround;
+
+    private Rigidbody2D myRigidBody2D;
     
 
 	// Use this for initialization
 	void Start ()
     {
-        string pizza = "yum";
-        Debug.Log(pizza); //how you print to the console in unity
+        myRigidBody2D = GetComponent<Rigidbody2D>(); //Searches for datatype in game and puts it in the variable so it's not a null reference. Variable must be initialized.
+        /*string pizza = "yum";
+        Debug.Log(pizza); //how you print to the console in unity*/
 
+        myRigidBody2D.gravityScale = 2;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //transform.Translate(0, -.01f, 0); //don't use, does not use physics. RigedBody does.
+        if (Input.GetKey(KeyCode.LeftArrow)) //GetKeyDown is just for the first time you hit it, GetKey lets you hold the key down
+        {
+            MoveLeft();
+        }
         
+        //transform.Translate(0, -.01f, 0); //don't use, does not use physics. RigedBody does.
+    }
+
+    private void MoveLeft()
+    {
+        myRigidBody2D.velocity = new Vector2(-5, 0);
     }
 }
