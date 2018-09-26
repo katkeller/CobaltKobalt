478899f476c40f89fe4f74964c5a2557a763b18b
Kathryn Keller <Kathryn.Keller@iam.local>
2018-09-26 14:36:31 -0500

    Messed with transform.translate

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    [SerializeField] //to expose a private int, only effects the one right under
+    [SerializeField] //to expose a private value, only effects the one right under, or multiple seperated by comma on same line.
     private int playerLives = 3;
 
     [SerializeField]
     private string playerName = "Steve";
 
+
+    private float jumpHeight = 1, speed = 5;
+
+    private bool hasKey; //bools must be named as yes/no questions, i.e. "hasKey" not "key."
+    private bool isOnGround;
+    
+
 	// Use this for initialization
 	void Start ()
     {
-        Debug.Log("yo."); //how you print to the console in unity
+        string pizza = "yum";
+        Debug.Log(pizza); //how you print to the console in unity
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        //transform.Translate(0, -.01f, 0); //don't use, does not use physics. RigedBody does.
+        
+    }
 }
