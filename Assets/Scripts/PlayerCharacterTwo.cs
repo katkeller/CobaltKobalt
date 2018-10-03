0392617b8d8233e25854132c2163af1d227e9291
Kathryn Keller <Kathryn.Keller@iam.local>
2018-10-03 13:52:22 -0500

    Added Max Acceleration

--- Assets/Scripts/PlayerCharacterTwo.cs
+++ Assets/Scripts/PlayerCharacterTwo.cs
@@ -6,8 +6,12 @@ public class PlayerCharacterTwo : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D rb2d;
+
     [SerializeField]
-    private float speed = 5;
+    private float accelerationForce = 5;
+
+    [SerializeField]
+    private float maxSpeed = 20;
 
     private float horizontalInput;
 	
@@ -23,6 +27,9 @@ public class PlayerCharacterTwo : MonoBehaviour
 
     void FixedUpdate ()
     {
-        rb2d.AddForce(Vector2.right * horizontalInput * speed);
+        rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVelocity = rb2d.velocity;
+        clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = clampedVelocity;
 	}
 }
