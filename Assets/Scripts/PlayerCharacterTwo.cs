c31c968eeb34f4f3d245424b3c1b41b49a49965d
Kathryn Keller <Kathryn.Keller@iam.local>
2018-10-03 13:23:02 -0500

    Made new Player Character Script and added it to Player Character

---
+++ Assets/Scripts/PlayerCharacterTwo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterTwo : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    private float horizontalInput;
+	
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+	}
+}
