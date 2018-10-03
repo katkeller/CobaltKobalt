3ef087bf418627ec78ecad00b4f837e8add0e955
Kathryn Keller <Kathryn.Keller@iam.local>
2018-10-03 14:20:01 -0500

    Added jump and changed physics materials

--- Assets/Scripts/PlayerCharacterTwo.cs
+++ Assets/Scripts/PlayerCharacterTwo.cs
@@ -13,23 +13,32 @@ public class PlayerCharacterTwo : MonoBehaviour
     [SerializeField]
     private float maxSpeed = 20;
 
+    [SerializeField]
+    private float jumpForce = 10;
+
     private float horizontalInput;
-	
-	void Start ()
-    {
-		
-	}
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     void FixedUpdate ()
+    {
+        Move();
+
+    }
+
+    private void Move()
     {
         rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
         Vector2 clampedVelocity = rb2d.velocity;
         clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
         rb2d.velocity = clampedVelocity;
-	}
+    }
 }
