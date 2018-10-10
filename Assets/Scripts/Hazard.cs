78b45a9efe03140f2b79856f3c21e9cf94e1907c
Kathryn Keller <Kathryn.Keller@iam.local>
2018-10-10 13:57:57 -0500

    Added crate and hazard script

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player entered Hazard");
+        }
+        else
+        {
+            Debug.Log("Something other than the player entered hazard");
+        }
+        
+    }
+}
