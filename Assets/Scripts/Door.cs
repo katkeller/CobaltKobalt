0931a763bf64dc05f287c6184b766fd16d3ee8ca
Kathryn Keller <Kathryn.Keller@iam.local>
2018-10-31 14:27:33 -0500

    Added door and door script with input detection and collision detection

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool isPlayerInTrigger;
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        if (Input.GetButtonDown("Activate"))
+    //        {
+    //            Debug.Log("Player activated door!");
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            Debug.Log("Player activated door!");
+        }
+    }
+}
