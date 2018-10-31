cd493b36d70702fd2e1352dcc31824217286fd11
Kathryn Keller <Kathryn.Keller@iam.local>
2018-10-31 13:20:26 -0500

    Deleted unused sprites, added checkpoint scaling

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -7,6 +7,9 @@ public class Checkpoint : MonoBehaviour
     [SerializeField]
     private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
 
+    [SerializeField]
+    private float inactivatedScale = 1, activatedScale = 1.5f;
+
     private bool isActivated;
 
     private void Update()
@@ -23,10 +26,21 @@ public class Checkpoint : MonoBehaviour
         }
         transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
+    private void UpdateScale()
+    {
+        float scale = inactivatedScale;
+
+        if (isActivated)
+        {
+            scale = activatedScale;
+        }
+        transform.localScale = Vector3.one * scale;
+    }
 
     public void SetIsActivated(bool value)
     {
         isActivated = value;
+        UpdateScale();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
