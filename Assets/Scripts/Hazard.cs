2560f95523938e56e791d0f9e8cb00f199c167ff
katkeller <43050286+katkeller@users.noreply.github.com>
2018-11-15 18:34:51 -0600

    Added red underline hazards and attempted to have death sound effect play

--- Assets/Scripts/Hazard.cs
+++ Assets/Scripts/Hazard.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Hazard : MonoBehaviour
 {
+    [SerializeField]
+    private float respawnDelay = 5.0f;
+
+    private AudioSource audioSource;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player entered Hazard");
+            audioSource.Play();
+            StartCoroutine(DelayRespawn());
             PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
             player.Respawn();
         }
@@ -18,5 +30,10 @@ public class Hazard : MonoBehaviour
             Debug.Log("Something other than the player entered hazard");
         }
         
+    }
+
+    IEnumerator DelayRespawn()
+    {
+        yield return new WaitForSeconds(respawnDelay);
     }
 }
