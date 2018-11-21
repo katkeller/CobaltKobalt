70e91883d98653a07a90066e35457708d9508eb8
Kathryn Keller <Kathryn.Keller@iam.local>
2018-11-21 14:51:44 -0600

    Created invisible checkpoints and added them to the scene

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,67 +4,67 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    [SerializeField]
-    private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
+    //[SerializeField]
+    //private float inactivatedRotationSpeed = 100, activatedRotationSpeed = 300;
 
-    [SerializeField]
-    private float inactivatedScale = 1, activatedScale = 1.5f;
+    //[SerializeField]
+    //private float inactivatedScale = 1, activatedScale = 1.5f;
 
-    [SerializeField]
-    private Color inactivatedColor, activatedColor;
+    //[SerializeField]
+    //private Color inactivatedColor, activatedColor;
 
-    private SpriteRenderer spriteRenderer;
-    private AudioSource audioSource;
+    //private SpriteRenderer spriteRenderer;
+    //private AudioSource audioSource;
     private bool isActivated;
 
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        audioSource = GetComponent<AudioSource>();
-        UpdateColor();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        //audioSource = GetComponent<AudioSource>();
+        //UpdateColor();
     }
 
     private void Update()
     {
-        UpdateRotation();
+        //UpdateRotation();
     }
 
-    private void UpdateRotation()
-    {
-        float rotationSpeed = inactivatedRotationSpeed;
-        if (isActivated)
-        {
-            rotationSpeed = activatedRotationSpeed;
-        }
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
-    }
-    private void UpdateScale()
-    {
-        float scale = inactivatedScale;
+    //private void UpdateRotation()
+    //{
+    //    float rotationSpeed = inactivatedRotationSpeed;
+    //    if (isActivated)
+    //    {
+    //        rotationSpeed = activatedRotationSpeed;
+    //    }
+    //    transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    //}
+    //private void UpdateScale()
+    //{
+    //    float scale = inactivatedScale;
 
-        if (isActivated)
-        {
-            scale = activatedScale;
-        }
-        transform.localScale = Vector3.one * scale;
-    }
+    //    if (isActivated)
+    //    {
+    //        scale = activatedScale;
+    //    }
+    //    transform.localScale = Vector3.one * scale;
+    //}
 
-    private void UpdateColor()
-    {
-        Color color = inactivatedColor;
+    //private void UpdateColor()
+    //{
+    //    Color color = inactivatedColor;
 
-        if (isActivated)
-        {
-            color = activatedColor;
-        }
-        spriteRenderer.color = color;
-    }
+    //    if (isActivated)
+    //    {
+    //        color = activatedColor;
+    //    }
+    //    spriteRenderer.color = color;
+    //}
 
     public void SetIsActivated(bool value)
     {
         isActivated = value;
-        UpdateScale();
-        UpdateColor();
+        //UpdateScale();
+        //UpdateColor();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -74,7 +74,7 @@ public class Checkpoint : MonoBehaviour
             Debug.Log("Player entered checkpoint");
             PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
             player.SetCurrentCheckpoint(this);
-            audioSource.Play();
+            //audioSource.Play();
         }
     }
 }
