c6d069b6fb4b220feeb33b4c83bcc48bbb160b57
Kathryn Keller <Kathryn.Keller@iam.local>
2018-11-28 13:11:53 -0600

    Added falling platform script and created falling platforms

---
+++ Assets/Scripts/FallingPlatforms.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatforms : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player activated falling tile!");
+            rb2d.isKinematic = false;
+        }
+    }
+}
